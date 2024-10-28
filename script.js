85621f58c6025bf8f5aa61c4acc603f7ac89ce7c
deeegreat6119 <anthonygoldpeter@gmail.com>
2024-10-28 08:06:06 -0400

    level one project

---
+++ script.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
